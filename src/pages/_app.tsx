5acb1891a2c12e52dcebad9afced2a2e6e01591a
Carlos <csnj@outlook.com>
2026-06-24 13:21:01 -0300

    pasta para o banco

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,6 @@ import '../styles/globals.css';
 import Nav from '../components/Nav';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Head from 'next/head';
-import fs from 'fs';
-
-// Cria a pasta db caso não exista.
-if (!fs.existsSync('db')) {
-  fs.mkdirSync('db');
-}
 
 const queryClient = new QueryClient();
 
